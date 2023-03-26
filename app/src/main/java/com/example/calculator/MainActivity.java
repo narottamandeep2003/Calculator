d4bce5c57f42ea4403eabe43dee9e9f0fd804117
Mandeep singh <narotta2003@gmail.com>
2023-03-26 20:25:45 +0530

    first commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CardView Calculator=findViewById(R.id.Calculator);
+        CardView UnitConverter=findViewById(R.id.UnitConverter);
+        CardView NumberConversion=findViewById(R.id.NumberConversion);
+        CardView LogicalOperation=findViewById(R.id.LogicalOperation);
+        CardView PermutationAndCombination=findViewById(R.id.PermutationAndCombination);
+//        Calculator
+        Calculator.setOnClickListener(view -> Log.d("S","Calculator"));
+//        UnitConverter
+        UnitConverter.setOnClickListener(view -> Log.d("S","UnitConverter"));
+//        NumberConversion
+        NumberConversion.setOnClickListener(view -> Log.d("S","NumberConversion"));
+//        LogicalOperation
+        LogicalOperation.setOnClickListener(view -> Log.d("S","LogicalOperation"));
+//        PermutationAndCombination
+        PermutationAndCombination.setOnClickListener(view -> Log.d("S","PermutationAndCombination"));
+
+
     }
 }
